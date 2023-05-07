87618bbdbea55df1cf54851813c01c94ab307b7a
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-05-06 22:06:38 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function Milk () {
+    return (
+        <>
+            <h1> Gott'emz Cream! </h1>
+        </>
+    );
+}
+
+ReactDom.render(<Milk />, document.getElementById('root'))
